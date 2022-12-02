0ea38ef99406b9f0173b98fcca976161991f61cd
Kanonaokiki <117688935+Kanonaokiki@users.noreply.github.com>
2022-12-03 02:23:41 +0900

    課題

--- Check2/src/check/Check.java
+++ Check2/src/check/Check.java
@@ -7,6 +7,7 @@ public class Check {
 	private static String firstName = "狩野";
 	private static String lastName = "尚輝";
 
+//	メソッドを作成し、引数に上記の変数を入れて返り文を作る。
 	private static String printName(String firstName, String lastName) {
 		//2番目の処理
 		return firstName + lastName;
@@ -15,11 +16,15 @@ public class Check {
 
 	public static void main(String[] args) {
 
+//		メソッドをstring型に代入し、変数を表示させる。
 		String Name = printName(firstName, lastName);
 		//1番目の処理
 		System.out.println("printNameメソッド → " + Name);
 		//4番目の処理
 
+//		インスタンス化をし、その中の引数に別クラスの定数を引き出してくる。その後、作成したものを
+//		メソッドで呼び出してくる。
+//		下の文も同様
 		Pet pet = new Pet(Constants.CHECK_CLASS_JAVA, Constants.CHECK_CLASS_HOGE);
 		pet.introduce();
 
--- Check2/src/check/Pet.java
+++ Check2/src/check/Pet.java
@@ -4,11 +4,12 @@ public class Pet {
     private String name;
     private String masterName;
 
+//コンストラクタ化
     public Pet(String name, String masterName) {
         this.name = name;
         this.masterName = masterName;
     }
-
+//コンストラクタ化の返り値
     protected String getName() {
         return name;
     }
@@ -16,18 +17,20 @@ public class Pet {
     protected String getMasterName() {
         return masterName;
     }
-
+//返り値をメソッドに入れる
     public void introduce() {
         System.out.println("■僕の名前は" + name + "です");
         System.out.println("■ご主人様は" + masterName + "です");
     }
 }
 
+//クラスを継承し、メソッドに継承先の変数をsuperで呼び出す。
 class RobotPet extends Pet {
     public RobotPet(String name, String masterName) {
         super(name, masterName);
     }
 
+  //返り値をメソッドに入れる
     public void introduce() {
         System.out.println("◇私はロボット。名前は" + getName() + "。");
         System.out.println("◇ご主人様は" + getMasterName() + "。");
